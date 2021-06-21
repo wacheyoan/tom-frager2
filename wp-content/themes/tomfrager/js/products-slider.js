b2006f40b351ad573f3afc8e3f9b3378251aee50
Yoan <y.wache@lemon-interactive.fr>
2021-06-21 10:40:03 +0200

    fixes

--- wp-content/themes/tomfrager/js/products-slider.js
+++ wp-content/themes/tomfrager/js/products-slider.js
@@ -1,17 +1,18 @@
 var nbrslider = 0;
 
-$(document).on("swipeleft","div.all-products", function (e) {
-    swipeHandler(-75)
-});
-$(document).on("swiperight","div.all-products", function (e) {
-    swipeHandler(75)
-});
+$(function (){
+    $("div.all-products").on("swipeleft", function (e) {
+        swipeHandler(-75)
+    });
+    $("div.all-products").on("swiperight", function (e) {
+        swipeHandler(75)
+    });
 
-function swipeHandler(number) {
-    nbrslider = nbrslider + number;
-    jQuery('.all-products').css('transform', 'translateX(' + nbrslider + '%)');
-}
-
+    function swipeHandler(number) {
+        nbrslider = nbrslider + number;
+        jQuery('.all-products').css('transform', 'translateX(' + nbrslider + '%)');
+    }
+})
 
 
 
